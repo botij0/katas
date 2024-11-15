7632c43e03bc81f46dfbc19bfeeab7ac0f4d5973
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-15 22:45:20 +0100

    location part 2

--- python/location/main.py
+++ python/location/main.py
@@ -4,6 +4,25 @@ from typing import List
 def main():
     file_content = read_file("puzzle_input.txt")
     print(process_movs(file_content))
+    print(process_robot_movs(file_content))
+
+
+def process_robot_movs(movs: str) -> int:
+    coordinates = [[0, 0]]
+    santa_init_coordinate = [0, 0]
+    robot_init_coordinate = [0, 0]
+    i = 0
+    for move in movs:
+        if i % 2 == 0:
+            santa_new_coordinate = get_new_coordinate(santa_init_coordinate, move)
+            if santa_new_coordinate not in coordinates:
+                coordinates.append(santa_new_coordinate.copy())
+        else:
+            robot_new_coordinate = get_new_coordinate(robot_init_coordinate, move)
+            if robot_new_coordinate not in coordinates:
+                coordinates.append(robot_new_coordinate.copy())
+        i += 1
+    return len(coordinates)
 
 
 def process_movs(movs: str) -> int:
