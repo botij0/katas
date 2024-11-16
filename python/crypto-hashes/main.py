a87105b2fb9e6629ed2a67febf2c5db0dca25375
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-16 12:14:07 +0100

    feat: crypto-hashes part 1

---
+++ python/crypto-hashes/main.py
@@ -0,0 +1,28 @@
+import hashlib
+
+PUZZLE_INPUT = "iwrupvqb"
+
+
+def main():
+    init_hash = get_md5(PUZZLE_INPUT)
+    i = 0
+    while not check_hash(init_hash):
+        init_hash = get_md5(PUZZLE_INPUT + str(i))
+        i += 1
+
+    print("Solution: " + str(i - 1))
+    print("Final Hash: " + init_hash)
+
+
+def check_hash(hash_input: str) -> bool:
+    return hash_input.startswith("00000")
+
+
+def get_md5(puzzle_input: str) -> str:
+    hash_md5 = hashlib.md5()
+    hash_md5.update(puzzle_input.encode("utf-8"))
+    return hash_md5.hexdigest()
+
+
+if __name__ == "__main__":
+    main()
