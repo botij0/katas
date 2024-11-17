de434f5c4428bb485a859bc272089de3c6740f7a
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-17 21:13:16 +0100

    feat: lighting config advances

---
+++ python/lighting_config/main.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+
+MAP = [[0] * 1000 for _ in range(1000)]
+
+
+def main():
+    file_content = read_file("puzzle_input.txt")
+    instruct_list = file_content.split("\n")
+    get_operation(instruct_list[0])
+
+
+def get_operation(s: str) -> dict:
+    inst_list = s.split(" ")
+    if len(operations) == 4:
+        instruct_dict = {
+            "operation": inst_list[0] + " " + inst_list[1],
+            "from_coordinates": 
+        }
+
+
+# Three Operations: turn off, turn on, toggle.
+
+
+def turn_on_light(row: int, col: int):
+    MAP[row][col] = 1
+
+
+def turn_off_light(row: int, col: int):
+    MAP[row][col] = 0
+
+
+def toggle_light(row: int, col: int):
+    MAP[row][col] = 1 if MAP[row][col] == 0 else 0
+
+
+def read_file(filename: str) -> str:
+    with open(filename, "r") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    main()
