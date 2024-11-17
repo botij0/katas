56a8247de33b8792b88335b1f87648b125e59de9
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-17 18:43:37 +0100

    feat: string verificator part 2

--- python/string_verificator/main.py
+++ python/string_verificator/main.py
@@ -3,9 +3,42 @@ def main():
     s_list = file_content.split("\n")
     nice_strings = 0
     for s in s_list:
-        if checker_string(s):
+        if new_checker(s):
             nice_strings += 1
     print(nice_strings)
+
+
+def new_checker(s: str) -> bool:
+    return check_letter_between(s) and check_pair(s)
+
+
+def check_letter_between(s: str) -> bool:
+    last_character = s[1]
+    last_2_character = s[0]
+    for i in range(2, len(s)):
+        if last_2_character == s[i]:
+            return True
+        last_2_character = last_character
+        last_character = s[i]
+    return False
+
+
+def check_pair(s: str) -> bool:
+    pares = [s[i : i + 2] for i in range(0, len(s) - 1)]
+    overlaping_flag = False
+    last_par = ""
+    for par in pares:
+        if par == last_par:
+            overlaping_flag = True
+        last_par = par
+
+    return len(pares) > len(set(pares)) and not overlaping_flag
+
+
+def checker_pair_letter_no_overlaping(s: str) -> bool:
+    twice_letter_flag = check_twice_letter(s)
+    s_list = list(s)
+    return twice_letter_flag is False and len(s_list) > len(set(s_list))
 
 
 def checker_string(s: str) -> bool:
@@ -29,6 +62,16 @@ def check_vowel(c: str) -> int:
     if c in vowels_list:
         return 1
     return 0
+
+
+def check_twice_letter(s: str) -> bool:
+    last_character = ""
+    for c in s:
+        if last_character == c:
+            return True
+        last_character = c
+
+    return False
 
 
 def check_no_contains(s: str) -> bool:
