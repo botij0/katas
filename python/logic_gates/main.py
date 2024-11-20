158ff281b72fd9ebea182cf07c9a6fe4016c604a
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-20 20:06:23 +0100

    feat: logic gates part 1

--- python/logic_gates/main.py
+++ python/logic_gates/main.py
@@ -8,27 +8,50 @@ def main():
     # and_gate(format(x, "016b"), format(y, "016b"))
     # or_gate(format(x, "016b"), format(y, "016b"))
     # lshift_gate(format(x, "016b"), 2)
-    file_content = read_file("example_input.txt")
+    # file_content = read_file("example_input.txt")
+    file_content = read_file("puzzle_input.txt")
     content = file_content.split("\n")
     wires = {}
-    for row in content:
-        aux = row.split(" ")
-        if len(aux) == 3:
-            wires[aux[-1]] = int(aux[0])
-        elif len(aux) == 4:
-            wires[aux[-1]] = not_gate(wires[aux[1]])
-        else:
-            match aux[1]:
-                case "AND":
-                    and_gate(wires[aux[0]], wires[aux[2]])
-                case "OR":
-                    or_gate(wires[aux[0]], wires[aux[2]])
-                case "LSHIFT":
-                    lshift_gate(wires[aux[0]], int(aux[2]))
-                case "RSHIFT":
-                    rshift_gate(wires[aux[0]], int(aux[2]))
+    while len(wires) < len(content):
+        for row in content:
+            aux = row.split(" ")
+            if len(aux) == 3:
+                val = get_value(aux[0], wires)
+                if val != -1:
+                    wires[aux[-1]] = val
+            elif len(aux) == 4:
+                val = get_value(aux[1], wires)
+                if val != -1:
+                    wires[aux[-1]] = not_gate(val)
 
+            else:
+                val_1 = get_value(aux[0], wires)
+                val_2 = get_value(aux[2], wires)
+                if val_1 != -1 and val_2 != -1:
+                    match aux[1]:
+                        case "AND":
+                            wires[aux[-1]] = and_gate(val_1, val_2)
+
+                        case "OR":
+                            wires[aux[-1]] = or_gate(val_1, val_2)
+
+                        case "LSHIFT":
+                            wires[aux[-1]] = lshift_gate(val_1, val_2)
+
+                        case "RSHIFT":
+                            wires[aux[-1]] = rshift_gate(val_1, val_2)
+
+    print(len(wires))
     print(wires)
+
+
+def get_value(s: str, wires: dict) -> int:
+    if is_int(s):
+        return int(s)
+    elif s in wires:
+        return wires[s]
+    else:
+        return -1
 
 
 def read_file(filename: str) -> str:
@@ -42,21 +65,27 @@ def not_gate(x: int):
 
 
 def and_gate(a: str, b: str):
-    return int(a, 2) & int(b, 2)
+    return a & b
 
 
 def or_gate(a: str, b: str):
-    return int(a, 2) | int(b, 2)
+    return a | b
 
 
 def lshift_gate(a: str, count: int):
-    aux = int(a, 2)
-    return aux << count
+    return a << count
 
 
 def rshift_gate(a: str, count: int):
-    aux = int(a, 2)
-    return aux >> count
+    return a >> count
+
+
+def is_int(s: str) -> bool:
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 
 # def rshift_gate(a: str, count: int):
