2b55c0837989de3e56df23b1c1d0d34ab66277fc
botij0 <javier.gonzalez.soldado@gmail.com>
2024-11-25 12:20:03 +0100

    feat: read_json_nums pt1

---
+++ python/read_json_nums/main.py
@@ -0,0 +1,55 @@
+import json
+
+EXAMPLE_RESULT = 18
+
+
+def main():
+    file = read_json("puzzle_input.json")
+    process_puzzle(file)
+
+
+def process_puzzle(file):
+    total = 0
+    for val in file:
+        if isinstance(val, list):
+            total += process_list(val)
+        elif isinstance(val, dict):
+            total += process_dict(val)
+
+    print(total)
+
+
+def process_list(x_list: list) -> int:
+    total = 0
+    for value in x_list:
+        if isinstance(value, int):
+            total += value
+
+        elif isinstance(value, list):
+            total += process_list(value)
+
+        elif isinstance(value, dict):
+            total += process_dict(value)
+    return total
+
+
+def process_dict(x_dict: dict) -> int:
+    total = 0
+    for value in x_dict.values():
+        if isinstance(value, int):
+            total += value
+        elif isinstance(value, list):
+            total += process_list(value)
+        elif isinstance(value, dict):
+            total += process_dict(value)
+
+    return total
+
+
+def read_json(filename: str) -> list:
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    main()
