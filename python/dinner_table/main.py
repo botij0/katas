cb0f2a8958b575febce2db8075884d41f16cd2ec
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-26 19:12:55 +0100

    feat: dinner_table pt1

--- python/dinner_table/main.py
+++ python/dinner_table/main.py
@@ -4,8 +4,8 @@ OPERATOR = {"gain": 1, "lose": -1}
 
 
 def main():
-    people = get_people_dict(read_file("example.txt"))
-    get_happiness(get_permutations(people), people)
+    people = get_people_dict(read_file("puzzle_input.txt"))
+    print(get_happiness(get_permutations(people), people))
 
 
 def read_file(filename: str) -> list:
@@ -32,8 +32,23 @@ def get_permutations(people: dict) -> list:
 def get_happiness(permutations: list, people: dict) -> int:
     max_happiness = 0
     for perm in permutations:
-        for name in perm:
-            print(name)
+        partial_happiness = 0
+
+        # Center Cases
+        for i in range(1, len(perm) - 1):
+            partial_happiness += people[perm[i]][perm[i + 1]]
+            partial_happiness += people[perm[i]][perm[i - 1]]
+
+        # Edges Cases
+        partial_happiness += people[perm[0]][perm[1]]
+        partial_happiness += people[perm[0]][perm[-1]]
+        partial_happiness += people[perm[-1]][perm[0]]
+        partial_happiness += people[perm[-1]][perm[-2]]
+
+        if max_happiness < partial_happiness:
+            max_happiness = partial_happiness
+
+    return max_happiness
 
 
 if __name__ == "__main__":
