5cb7497f27ea3689ec5aae8c24193bc395d53e7a
botij0 <javier.gonzalez.soldado@gmail.com>
2024-11-27 15:11:54 +0100

    feat: reindeer olympics pt2

--- python/reindeer_olympics/main.py
+++ python/reindeer_olympics/main.py
@@ -1,9 +1,49 @@
 def main():
     reindeers = get_reindeer_dict(read_file("puzzle_input.txt"))
-    proccess_reindeers(reindeers, 2503)
+    process_reindeers_pt2(reindeers, 2503)
 
 
-def proccess_reindeers(reindeers: dict, max_seconds: int):
+def process_reindeers_pt2(reindeers: dict, max_seconds: int):
+    for sec in range(1, max_seconds + 1):
+        for key in reindeers.keys():
+            if (
+                reindeers[key]["partial_count"]
+                >= reindeers[key]["time"] + reindeers[key]["cooldown"]
+            ):
+                reindeers[key]["partial_count"] = 0
+
+            if not (
+                reindeers[key]["partial_count"] >= reindeers[key]["time"]
+                and reindeers[key]["partial_count"]
+                < (reindeers[key]["time"] + reindeers[key]["cooldown"])
+            ):
+                reindeers[key]["distance"] += reindeers[key]["speed"]
+
+            reindeers[key]["partial_count"] += 1
+        give_point(reindeers)
+
+    for key in reindeers:
+        print(f"{key} - {reindeers[key]["point"]}")
+
+
+def give_point(reindeers: dict):
+    winners = get_winner(reindeers)
+    for winner in winners:
+        reindeers[winner]["point"] += 1
+
+
+def get_winner(reindeers: dict) -> list:
+    distances = sorted([reindeers[key]["distance"] for key in reindeers])
+    max_distance = distances[-1]
+    winners = []
+    for key in reindeers.keys():
+        if reindeers[key]["distance"] == max_distance:
+            winners.append(key)
+
+    return winners
+
+
+def proccess_reindeers_pt1(reindeers: dict, max_seconds: int):
     for key in reindeers.keys():
         partial_time = reindeers[key]["time"] + reindeers[key]["cooldown"]
         cicles = int(max_seconds / partial_time)
@@ -30,6 +70,8 @@ def get_reindeer_dict(content: list) -> dict:
             "time": int(aux[6]),
             "cooldown": int(aux[-2]),
             "distance": 0,
+            "partial_count": 0,
+            "point": 0,
         }
     return reindeers
 
