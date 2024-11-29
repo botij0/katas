3b330b6ad626c24ba3ab178adbe1c605eec9543a
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-29 18:55:43 +0100

    feat: cookie recipe pt1

--- python/cookie-recipe/main.py
+++ python/cookie-recipe/main.py
@@ -1,5 +1,3 @@
-from itertools import combinations_with_replacement
-
 MAX = 100
 
 
@@ -8,6 +6,37 @@ def main():
     ingredients_dict = get_ingredients_dict(ingredients)
     print(ingredients_dict)
     get_better_cuantity(ingredients_dict)
+
+
+def get_better_cuantity(ingredients_dict: dict):
+    max_score = 0
+    best_comb = None
+    for x in range(1, 101):
+        for y in range(1, 101 - x):
+            for z in range(1, 101 - x - y):
+                q = 100 - x - y - z
+                score = calculate_ingredients_score(ingredients_dict, [x, y, z, q])
+
+                if score > max_score:
+                    max_score = score
+                    best_comb = [x, y, z, q]
+
+    print(best_comb)
+    print(max_score)
+
+
+def calculate_ingredients_score(ingredients_dict: dict, comb: list[int]) -> int:
+    capacity = 0
+    durability = 0
+    flavor = 0
+    texture = 0
+    for i, key in enumerate(ingredients_dict.keys()):
+        capacity += ingredients_dict[key]["capacity"] * comb[i]
+        durability += ingredients_dict[key]["durability"] * comb[i]
+        flavor += ingredients_dict[key]["flavor"] * comb[i]
+        texture += ingredients_dict[key]["texture"] * comb[i]
+
+    return get_score(capacity, durability, flavor, texture)
 
 
 def get_ingredients_dict(ingredients: list) -> dict:
@@ -26,36 +55,8 @@ def get_ingredients_dict(ingredients: list) -> dict:
     return ingredients_dict
 
 
-def get_better_cuantity(ingredients_dict: dict):
-    combinations = [
-        comb
-        for comb in combinations_with_replacement(range(1, 101), 4)
-        if sum(comb) == MAX
-    ]
-    max_score = 0
-    best_comb = None
-    for comb in combinations:
-        capacity = 0
-        durability = 0
-        flavor = 0
-        texture = 0
-        for i, key in enumerate(ingredients_dict.keys()):
-            capacity += ingredients_dict[key]["capacity"] * comb[i]
-            durability += ingredients_dict[key]["durability"] * comb[i]
-            flavor += ingredients_dict[key]["flavor"] * comb[i]
-            texture += ingredients_dict[key]["texture"] * comb[i]
-
-        score = get_score(capacity, durability, flavor, texture)
-        if score > max_score:
-            max_score = score
-            best_comb = comb
-
-    print(best_comb)
-    print(max_score)
-
-
 def get_score(capacity: int, durability: int, flavor: int, texture: int) -> int:
-    if capacity == 0 or durability == 0 or flavor == 0 or texture == 0:
+    if capacity <= 0 or durability <= 0 or flavor <= 0 or texture <= 0:
         return 0
     return capacity * durability * flavor * texture
 
