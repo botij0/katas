863466e3270a7f602782c297c11f9eeef8c8daff
botij0 <javier.gonzalez.soldado@gmail.com>
2024-12-03 17:17:09 +0100

    feat: mull-it-over pt2 Advances

--- python/mull-it-over/main.py
+++ python/mull-it-over/main.py
@@ -2,11 +2,57 @@ import re
 
 
 def main():
-    file_content = read_file("example.txt")
-
+    file_content = read_file("example_pt2.txt")
+    total_dict = (
+        get_dos_dict(file_content)
+        | get_dont_dict(file_content)
+        | get_mults_dict(file_content)
+    )
+    dict_ordenado = {k: total_dict[k] for k in sorted(total_dict)}
+    print(dict_ordenado)
     # pt 1
-    nums = get_multiplications(file_content)
-    print(get_result(nums))
+    # nums = get_multiplications(file_content)
+    # print(get_result(nums))
+
+
+def process_dict(dict_ordenado: dict) -> int:
+    disabled = False
+    total = 0
+    for value in dict_ordenado.values():
+        if value == "do()":
+            disabled = True
+        elif value == "don't()":
+            disabled = False
+        elif value == "mul()":
+            if not disabled:
+                total += 1
+
+
+def get_dos_dict(file_content: str) -> dict:
+    dos_dict = {}
+    start = 0
+    while (index := file_content.find("do()", start)) != -1:
+        dos_dict[index] = "do()"
+        start = index + 1
+    return dos_dict
+
+
+def get_dont_dict(file_content: str) -> dict:
+    dont_dict = {}
+    start = 0
+    while (index := file_content.find("don't()", start)) != -1:
+        dont_dict[index] = "don't()"
+        start = index + 1
+    return dont_dict
+
+
+def get_mults_dict(file_content: str) -> dict:
+    mult_dicts = {}
+    patron = re.compile(r"mul\((\d+),(\d+)\)")
+    matches = [match.start() for match in patron.finditer(file_content)]
+    for match in matches:
+        mult_dicts[match] = "mul()"
+    return mult_dicts
 
 
 def get_result(nums: list) -> int:
