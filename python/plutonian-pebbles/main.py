248fedddc7cfe6ee3a6f0495cc24b3470e1f7edc
botij0 <javier.gonzalez.soldado@gmail.com>
2024-12-12 10:04:52 +0100

    feat: plutonian-pebbles pt1

--- python/plutonian-pebbles/main.py
+++ python/plutonian-pebbles/main.py
@@ -1,6 +1,24 @@
 def main():
-    file_content = read_file("example.txt")
-    print(file_content)
+    file_content = read_file("puzzle_input.txt")
+    stones = [int(x) for x in file_content.split()]
+    for i in range(75):
+        stones = proccess_stones_iteration(stones)
+    print(len(stones))
+
+
+def proccess_stones_iteration(stones: list) -> list:
+    aux = []
+    for stone in stones:
+        if stone == 0:
+            aux.append(1)
+        elif len(str(stone)) % 2 == 0:
+            stone_str = str(stone)
+            aux.append(int(stone_str[0 : int(len(stone_str) / 2)]))
+            aux.append(int(stone_str[int(len(stone_str) / 2) :]))
+        else:
+            aux.append(stone * 2024)
+
+    return aux
 
 
 def read_file(filename: str):
