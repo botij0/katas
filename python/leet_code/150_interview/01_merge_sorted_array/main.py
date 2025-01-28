f8d8017c859ca28d742a216967dc2e5964d58b7e
botij0 <javier.gonzalez.soldado@gmail.com>
2025-01-28 12:19:25 +0100

    feat: leet_code 01

---
+++ python/leet_code/150_interview/01_merge_sorted_array/main.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def merge(self, nums1: list, m: int, nums2: list, n: int):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m, m + n):
+            nums1[i] = nums2[i - m]
+        nums1.sort()
+
+
+if __name__ == "__main__":
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    obj = Solution()
+    obj.merge(nums1, m, nums2, n)
+    print(nums1)
