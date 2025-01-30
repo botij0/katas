9c760cf0eb5bcaf0b41e8e72ccc43ad72ef0f624
botij0 <javier.gonzalez.soldado@gmail.com>
2025-01-30 16:51:04 +0100

    feat: leet_code 200 best

--- python/leet_code/150_interview/200_number_of_islands/main.py
+++ python/leet_code/150_interview/200_number_of_islands/main.py
@@ -6,43 +6,25 @@ class Solution(object):
         """
         n_rows = len(grid)
         n_cols = len(grid[0])
-        islands = []
+        res = 0
         for i in range(n_rows):
             for j in range(n_cols):
-                if grid[i][j] == "0":
-                    continue
+                if grid[i][j] == "1":
+                    calculate_island(i, j, n_rows, n_cols, grid)
+                    res += 1
 
-                if check_if_new_island(islands, i, j):
-                    new_island = set()
-                    calculate_island(i, j, n_rows, n_cols, grid, new_island)
-                    islands.append(new_island)
-
-        return len(islands)
+        return res
 
 
-def check_if_new_island(islands: set, i, j) -> bool:
-    for island in islands:
-        if (i, j) in island:
-            return False
-    return True
-
-
-def calculate_island(i: int, j: int, n_rows: int, n_cols: int, grid: list, island: set):
-    if (
-        (i, j) in island
-        or i <= -1
-        or i >= n_rows
-        or j <= -1
-        or j >= n_cols
-        or grid[i][j] == "0"
-    ):
+def calculate_island(i: int, j: int, n_rows: int, n_cols: int, grid: list):
+    if i <= -1 or i >= n_rows or j <= -1 or j >= n_cols or grid[i][j] == "0":
         return
 
-    island.add((i, j))
-    calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-    calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-    calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-    calculate_island(i, j + 1, n_rows, n_cols, grid, island)
+    grid[i][j] = "0"
+    calculate_island(i - 1, j, n_rows, n_cols, grid)
+    calculate_island(i + 1, j, n_rows, n_cols, grid)
+    calculate_island(i, j - 1, n_rows, n_cols, grid)
+    calculate_island(i, j + 1, n_rows, n_cols, grid)
 
 
 if __name__ == "__main__":
