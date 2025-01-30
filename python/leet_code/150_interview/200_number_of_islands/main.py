fcf1069275340757bd5f1d4f93abcca9959840a1
botij0 <javier.gonzalez.soldado@gmail.com>
2025-01-30 16:42:21 +0100

    feat: leet_code 200 better

--- python/leet_code/150_interview/200_number_of_islands/main.py
+++ python/leet_code/150_interview/200_number_of_islands/main.py
@@ -28,83 +28,28 @@ def check_if_new_island(islands: set, i, j) -> bool:
 
 
 def calculate_island(i: int, j: int, n_rows: int, n_cols: int, grid: list, island: set):
-    if (i, j) in island:
+    if (
+        (i, j) in island
+        or i <= -1
+        or i >= n_rows
+        or j <= -1
+        or j >= n_cols
+        or grid[i][j] == "0"
+    ):
         return
 
     island.add((i, j))
-    if is_a_corner(i, j, n_cols, n_rows):
-        if i == 0 and j == 0:
-            if n_rows > 1 and grid[i + 1][j] == "1":
-                calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j + 1] == "1":
-                calculate_island(i, j + 1, n_rows, n_cols, grid, island)
-        elif i == 0 and j == n_cols - 1:
-            if n_rows > 1 and grid[i + 1][j] == "1":
-                calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j - 1] == "1":
-                calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-        elif i == n_rows - 1 and j == 0:
-            if n_rows > 1 and grid[i - 1][j] == "1":
-                calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j + 1] == "1":
-                calculate_island(i, j + 1, n_rows, n_cols, grid, island)
-        elif i == n_rows - 1 and j == n_cols - 1:
-            if n_rows > 1 and grid[i - 1][j] == "1":
-                calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j - 1] == "1":
-                calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-
-    elif is_an_edge(i, j, n_cols, n_rows):
-        if i == 0:
-            if n_rows > 1 and grid[i + 1][j] == "1":
-                calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j + 1] == "1":
-                calculate_island(i, j + 1, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j - 1] == "1":
-                calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-        if i == n_rows - 1:
-            if n_rows > 1 and grid[i - 1][j] == "1":
-                calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j + 1] == "1":
-                calculate_island(i, j + 1, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j - 1] == "1":
-                calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-        if j == 0:
-            if n_rows > 1 and grid[i - 1][j] == "1":
-                calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j + 1] == "1":
-                calculate_island(i, j + 1, n_rows, n_cols, grid, island)
-            if n_rows > 1 and grid[i + 1][j] == "1":
-                calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-        if j == n_cols - 1:
-            if n_rows > 1 and grid[i - 1][j] == "1":
-                calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-            if n_cols > 1 and grid[i][j - 1] == "1":
-                calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-            if n_rows > 1 and grid[i + 1][j] == "1":
-                calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-
-    else:
-        if grid[i][j + 1] == "1":
-            calculate_island(i, j + 1, n_rows, n_cols, grid, island)
-        if grid[i][j - 1] == "1":
-            calculate_island(i, j - 1, n_rows, n_cols, grid, island)
-        if grid[i + 1][j] == "1":
-            calculate_island(i + 1, j, n_rows, n_cols, grid, island)
-        if grid[i - 1][j] == "1":
-            calculate_island(i - 1, j, n_rows, n_cols, grid, island)
-
-
-def is_a_corner(i: int, j: int, n_cols: int, n_rows: int) -> bool:
-    return (i == 0 and (j == 0 or j == n_cols - 1)) or (
-        i == n_rows - 1 and (j == 0 or j == n_cols - 1)
-    )
-
-
-def is_an_edge(i: int, j: int, n_cols: int, n_rows: int) -> bool:
-    return i == 0 or j == 0 or i == n_rows - 1 or j == n_cols - 1
+    calculate_island(i - 1, j, n_rows, n_cols, grid, island)
+    calculate_island(i + 1, j, n_rows, n_cols, grid, island)
+    calculate_island(i, j - 1, n_rows, n_cols, grid, island)
+    calculate_island(i, j + 1, n_rows, n_cols, grid, island)
 
 
 if __name__ == "__main__":
-    grid = [["1", "1"]]
+    grid = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"],
+    ]
     print(Solution().numIslands(grid))
