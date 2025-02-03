977d8fedd13870c3cb399ffbce98ba3874534d27
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2025-02-03 20:07:34 +0100

    feat: go back

--- python/leet_code/150_interview/843_Guess_The_Word/main.py
+++ python/leet_code/150_interview/843_Guess_The_Word/main.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Master:
     def __init__(self, secret: str, allowedGuesses: int):
         self.secret = secret
@@ -25,13 +22,12 @@ class Solution(object):
         :type master: Master
         :rtype: None
         """
-        i = 0
+        words.sort()
         tries = 0
         coincidence_word = ""
-        current_secret = {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""}
         max_coincidence = 0
         bad_letters = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
-        for word in random.sample(words, len(words)):
+        for word in words:
             if tries >= master.allowedGuesses:
                 print("you failed")
                 return
@@ -43,13 +39,13 @@ class Solution(object):
                 max_coincidence,
                 coincidence_word,
                 tries,
-                current_secret,
             )
 
             if max_coincidence == 6:
                 print(
                     "You guessed the secret word correctly. Secret word is",
                     coincidence_word,
+                    tries,
                 )
                 return
 
@@ -61,10 +57,9 @@ class Solution(object):
         max_coincidence: int,
         coincidence_word: str,
         tries: int,
-        current_secret: dict,
     ):
         if self.should_try_new_word(
-            word, coincidence_word, max_coincidence, bad_letters, current_secret
+            word, coincidence_word, max_coincidence, bad_letters
         ):
             current_coincidences = master.guess(word)
             tries += 1
@@ -72,17 +67,6 @@ class Solution(object):
 
             if current_coincidences == 0:
                 self.update_bad_letters(coincidence_word, word, bad_letters)
-                self.clean_current_secret(word, current_secret)
-
-            if max_coincidence != 0 and current_coincidences == max_coincidence:
-                self.update_current_secret(word, coincidence_word, current_secret)
-                if max_coincidence == 1:
-                    print(current_secret)
-                    if self.validate_secret(current_secret, master) is False:
-                        print("not validddddddd")
-                        self.clean_current_secret(word, current_secret)
-                    print(current_secret)
-                    tries += 1
 
             if current_coincidences == 6:
                 return word, current_coincidences, tries
@@ -99,23 +83,12 @@ class Solution(object):
         coincidence_word: str,
         max_coincidences: int,
         bad_letters: dict,
-        current_secret: dict,
     ):
-        if self.check_current_secret(word, current_secret):
-            return False
         if self.check_bad_letters(word, bad_letters):
             return False
         if not self.check_same_coincidences(coincidence_word, word, max_coincidences):
             return False
         return True
-
-    def check_current_secret(self, word: str, current_secret: dict) -> bool:
-        print(word, current_secret)
-        for i in range(len(word)):
-            if current_secret[i] != "":
-                if word[i] != current_secret[i]:
-                    return True
-        return False
 
     def check_bad_letters(self, word: str, bad_letters: dict) -> bool:
         bad_letters_count = 0
@@ -153,136 +126,109 @@ class Solution(object):
                 bad_letters_coincidence.append((i, coincidence_word[i]))
         return bad_letters_coincidence
 
-    def update_current_secret(
-        self, word: str, coincidence_word: str, current_secret: dict
-    ):
-        for i in range(len(word)):
-            if word[i] == coincidence_word[i]:
-                current_secret[i] = word[i]
-
-    def clean_current_secret(self, word: str, current_secret: dict):
-        for i in range(len(word)):
-            if word[i] == current_secret[i]:
-                current_secret[i] = ""
-
-    def validate_secret(self, current_secret: dict, master):
-        secret_try = ""
-        for val in current_secret.values():
-            if val != "":
-                secret_try += val
-            else:
-                secret_try += "*"
-
-        aux = master.guess(secret_try)
-        print(secret_try, aux)
-        if aux >= 1:
-            return True
-        else:
-            return False
-
 
 if __name__ == "__main__":
-    master = Master("anqomr", 11)
+    master = Master("vftnkr", 12)
     words = [
-        "pzrooh",
-        "aaakrw",
-        "vgvkxb",
-        "ilaumf",
-        "snzsrz",
-        "qymapx",
-        "hhjgwz",
-        "mymxyu",
-        "jglmrs",
-        "yycsvl",
-        "fuyzco",
-        "ivfyfx",
-        "hzlhqt",
-        "ansstc",
-        "ujkfnr",
-        "jrekmp",
-        "himbkv",
-        "tjztqw",
-        "jmcapu",
-        "gwwwmd",
-        "ffpond",
-        "ytzssw",
-        "afyjnp",
-        "ttrfzi",
-        "xkwmsz",
-        "oavtsf",
-        "krwjwb",
-        "bkgjcs",
-        "vsigmc",
-        "qhpxxt",
-        "apzrtt",
-        "posjnv",
-        "zlatkz",
-        "zynlqc",
-        "czajmi",
-        "smmbhm",
-        "rvlxav",
-        "wkytta",
-        "dzkfer",
-        "urajfh",
-        "lsroct",
-        "gihvuu",
-        "qtnlhu",
-        "ucjgio",
-        "xyycvd",
-        "fsssoo",
-        "kdtmux",
-        "bxnppe",
-        "usucra",
-        "hvsmau",
-        "gstvvg",
-        "ypueay",
-        "qdlvod",
-        "djfbgs",
-        "mcufib",
-        "prohkc",
-        "dsqgms",
-        "eoasya",
-        "kzplbv",
-        "rcuevr",
-        "iwapqf",
-        "ucqdac",
-        "anqomr",
-        "msztnf",
-        "tppefv",
-        "mplbgz",
-        "xnskvo",
-        "euhxrh",
-        "xrqxzw",
-        "wraxvn",
-        "zjhlou",
-        "xwdvvl",
-        "dkbiys",
-        "zbtnuv",
-        "gxrhjh",
-        "ctrczk",
-        "iwylwn",
-        "wefuhr",
-        "enlcrg",
-        "eimtep",
-        "xzvntq",
-        "zvygyf",
-        "tbzmzk",
-        "xjptby",
-        "uxyacb",
-        "mbalze",
-        "bjosah",
-        "ckojzr",
-        "ihboso",
-        "ogxylw",
-        "cfnatk",
-        "zijwnl",
-        "eczmmc",
-        "uazfyo",
-        "apywnl",
-        "jiraqa",
-        "yjksyd",
-        "mrpczo",
-        "qqmhnb",
-        "xxvsbx",
+        "mjpsce",
+        "giwiyk",
+        "slbnia",
+        "pullbr",
+        "ezvczd",
+        "dwkrmt",
+        "qgzebh",
+        "wvhhlm",
+        "kqbmny",
+        "zpvrkz",
+        "pdwxvy",
+        "gilywa",
+        "gmrrdc",
+        "vvqvla",
+        "rmjirt",
+        "qmvykq",
+        "mhbmuq",
+        "unplzn",
+        "qkcied",
+        "eignxg",
+        "fbfgng",
+        "xpizga",
+        "twubzr",
+        "nnfaxr",
+        "skknhe",
+        "twautl",
+        "nglrst",
+        "mibyks",
+        "qrbmpx",
+        "ukgjkq",
+        "mhxxfb",
+        "deggal",
+        "bwpvsp",
+        "uirtak",
+        "tqkzfk",
+        "hfzawa",
+        "jahjgn",
+        "mteyut",
+        "jzbqbv",
+        "ttddtf",
+        "auuwgn",
+        "untihn",
+        "gbhnog",
+        "zowaol",
+        "feitjl",
+        "omtiur",
+        "kwdsgx",
+        "tggcqq",
+        "qachdn",
+        "dixtat",
+        "hcsvbw",
+        "chduyy",
+        "gpdtft",
+        "bjxzky",
+        "uvvvko",
+        "jzcpiv",
+        "gtyjau",
+        "unsmok",
+        "vfcmhc",
+        "hvxnut",
+        "orlwku",
+        "ejllrv",
+        "jbrskt",
+        "xnxxdi",
+        "rfreiv",
+        "njbvwj",
+        "pkydxy",
+        "jksiwj",
+        "iaembk",
+        "pyqdip",
+        "exkykx",
+        "uxgecc",
+        "khzqgy",
+        "dehkbu",
+        "ahplng",
+        "jomiik",
+        "nmcsfe",
+        "bclcbp",
+        "xfiefi",
+        "soiwde",
+        "tcjkjp",
+        "wervlz",
+        "dcthgv",
+        "hwwghe",
+        "hdlkll",
+        "dpzoxb",
+        "mpiviy",
+        "cprcwo",
+        "molttv",
+        "dwjtdp",
+        "qiilsr",
+        "dbnaxs",
+        "dbozaw",
+        "webcyp",
+        "vftnkr",
+        "iurlzf",
+        "giqcfc",
+        "pcghoi",
+        "zupyzn",
+        "xckegy",
     ]
     Solution().findSecretWord(words, master)
