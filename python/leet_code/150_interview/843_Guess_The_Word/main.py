ede4855eb86e6b3d3edc75486b9b2ed2cbfa5682
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-03 19:30:52 +0100

    feat: more improves

--- python/leet_code/150_interview/843_Guess_The_Word/main.py
+++ python/leet_code/150_interview/843_Guess_The_Word/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Master:
     def __init__(self, secret: str, allowedGuesses: int):
         self.secret = secret
@@ -22,26 +25,25 @@ class Solution(object):
         :type master: Master
         :rtype: None
         """
-        words.sort()
+        i = 0
         tries = 0
         coincidence_word = ""
+        current_secret = {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""}
         max_coincidence = 0
-        bad_words = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
-        for i in range(len(words)):
+        bad_letters = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
+        for word in random.sample(words, len(words)):
             if tries >= master.allowedGuesses:
                 print("you failed")
                 return
 
             coincidence_word, max_coincidence, tries = self.word_try(
-                words[i], master, bad_words, max_coincidence, coincidence_word, tries
-            )
-            coincidence_word, max_coincidence, tries = self.word_try(
-                words[len(words) - i - 1],
+                word,
                 master,
-                bad_words,
+                bad_letters,
                 max_coincidence,
                 coincidence_word,
                 tries,
+                current_secret,
             )
 
             if max_coincidence == 6:
@@ -55,182 +57,232 @@ class Solution(object):
         self,
         word: str,
         master: Master,
-        bad_words: dict,
+        bad_letters: dict,
         max_coincidence: int,
         coincidence_word: str,
         tries: int,
+        current_secret: dict,
     ):
-        if self.should_try_new_word(word, coincidence_word, max_coincidence, bad_words):
-            x = master.guess(word)
+        if self.should_try_new_word(
+            word, coincidence_word, max_coincidence, bad_letters, current_secret
+        ):
+            current_coincidences = master.guess(word)
             tries += 1
-            print(word, coincidence_word, tries, max_coincidence, x)
+            print(word, coincidence_word, tries, max_coincidence, current_coincidences)
 
-            if x == 0:
-                if len(coincidence_word) > 0:
-                    bad_letters = self.get_bad_letters(coincidence_word, word)
-                    for letter in bad_letters:
-                        bad_words[letter[0]].append(letter[1])
+            if current_coincidences == 0:
+                self.update_bad_letters(coincidence_word, word, bad_letters)
+                self.clean_current_secret(word, current_secret)
 
-                else:
-                    for i in range(6):
-                        bad_words[i].append(word[i])
+            if max_coincidence != 0 and current_coincidences == max_coincidence:
+                self.update_current_secret(word, coincidence_word, current_secret)
+                if max_coincidence == 1:
+                    print(current_secret)
+                    if self.validate_secret(current_secret, master) is False:
+                        print("not validddddddd")
+                        self.clean_current_secret(word, current_secret)
+                    print(current_secret)
+                    tries += 1
 
-            if x < max_coincidence:
-                coincidence_word = self.modify_coincidenc_word(coincidence_word, word)
+            if current_coincidences == 6:
+                return word, current_coincidences, tries
 
-            if x == 6:
-                return word, x, tries
-
-            if x > max_coincidence:
-                max_coincidence = x
+            if current_coincidences > max_coincidence:
+                max_coincidence = current_coincidences
                 coincidence_word = word
 
         return coincidence_word, max_coincidence, tries
 
     def should_try_new_word(
-        self, word: str, coincidence_word: str, max_coincidences: int, bad_words: dict
+        self,
+        word: str,
+        coincidence_word: str,
+        max_coincidences: int,
+        bad_letters: dict,
+        current_secret: dict,
     ):
-        bad_letters = 0
-        for i, c in enumerate(word):
-            if c in bad_words[i]:
-                bad_letters += 1
-
-        if bad_letters > 0:
+        if self.check_current_secret(word, current_secret):
             return False
+        if self.check_bad_letters(word, bad_letters):
+            return False
+        if not self.check_same_coincidences(coincidence_word, word, max_coincidences):
+            return False
+        return True
 
+    def check_current_secret(self, word: str, current_secret: dict) -> bool:
+        print(word, current_secret)
+        for i in range(len(word)):
+            if current_secret[i] != "":
+                if word[i] != current_secret[i]:
+                    return True
+        return False
+
+    def check_bad_letters(self, word: str, bad_letters: dict) -> bool:
+        bad_letters_count = 0
+        for i, c in enumerate(word):
+            if c in bad_letters[i]:
+                bad_letters_count += 1
+
+        return True if bad_letters_count > 0 else False
+
+    def check_same_coincidences(
+        self, coincidence_word: str, word: str, max_coincidences: int
+    ) -> bool:
         coincidences = 0
         for i in range(len(coincidence_word)):
-            if coincidence_word[i].isupper():
-                if word[i].upper() != coincidence_word[i]:
-                    coincidences = -1
-                    break
-                else:
-                    coincidences += 1
-
             if word[i] == coincidence_word[i]:
                 coincidences += 1
 
         return True if coincidences == max_coincidences else False
 
-    def get_bad_letters(self, coincidence_word: str, word: str):
-        bad_letters = []
+    def update_bad_letters(self, coincidence_word: str, word: str, bad_letters: dict):
+        if len(coincidence_word) > 0:
+            bad_letters_coinc = self.get_bad_letters_coincidence(coincidence_word, word)
+            for letter in bad_letters_coinc:
+                bad_letters[letter[0]].append(letter[1])
+
+        else:
+            for i in range(6):
+                bad_letters[i].append(word[i])
+
+    def get_bad_letters_coincidence(self, coincidence_word: str, word: str) -> list:
+        bad_letters_coincidence = []
 
         for i in range(len(word)):
             if word[i] == coincidence_word[i]:
-                bad_letters.append((i, coincidence_word[i]))
-        return bad_letters
+                bad_letters_coincidence.append((i, coincidence_word[i]))
+        return bad_letters_coincidence
 
-    def modify_coincidenc_word(self, coincidence_word: str, word: str):
-        new_word = list(coincidence_word)
-
+    def update_current_secret(
+        self, word: str, coincidence_word: str, current_secret: dict
+    ):
         for i in range(len(word)):
-            if word[i] != coincidence_word[i]:
-                new_word[i] = coincidence_word[i].capitalize()
-        return "".join(new_word)
+            if word[i] == coincidence_word[i]:
+                current_secret[i] = word[i]
+
+    def clean_current_secret(self, word: str, current_secret: dict):
+        for i in range(len(word)):
+            if word[i] == current_secret[i]:
+                current_secret[i] = ""
+
+    def validate_secret(self, current_secret: dict, master):
+        secret_try = ""
+        for val in current_secret.values():
+            if val != "":
+                secret_try += val
+            else:
+                secret_try += "*"
+
+        aux = master.guess(secret_try)
+        print(secret_try, aux)
+        if aux >= 1:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
-    master = Master("aaaata", 30)
+    master = Master("anqomr", 11)
     words = [
-        "aaaaga",
-        "aaaaka",
-        "aaauaa",
-        "aaaaoa",
-        "aafaaa",
-        "aaaaza",
-        "aaaava",
-        "agaaaa",
-        "aaagaa",
-        "aaaaqa",
-        "aaaaca",
-        "aaaaua",
-        "apaaaa",
-        "aawaaa",
-        "aaaaba",
-        "aaaqaa",
-        "aayaaa",
-        "aaaaja",
-        "aaacaa",
-        "aaayaa",
-        "aaaeaa",
-        "aavaaa",
-        "aasaaa",
-        "aaaapa",
-        "aaaaxa",
-        "aeaaaa",
-        "aaxaaa",
-        "akaaaa",
-        "aaaoaa",
-        "aazaaa",
-        "anaaaa",
-        "aaaala",
-        "aaraaa",
-        "aaaata",
-        "aaaaia",
-        "ajaaaa",
-        "aaaaaa",
-        "ahaaaa",
-        "aaaraa",
-        "aaaiaa",
-        "aanaaa",
-        "alaaaa",
-        "aakaaa",
-        "aiaaaa",
-        "aajaaa",
-        "aaakaa",
-        "axaaaa",
-        "aaqaaa",
-        "aaamaa",
-        "aapaaa",
-        "aaafaa",
-        "aaasaa",
-        "aadaaa",
-        "amaaaa",
-        "aaaaea",
-        "aabaaa",
-        "aaaama",
-        "asaaaa",
-        "acaaaa",
-        "aaiaaa",
-        "avaaaa",
-        "afaaaa",
-        "aoaaaa",
-        "aamaaa",
-        "aaaasa",
-        "aaawaa",
-        "azaaaa",
-        "aataaa",
-        "aaeaaa",
-        "aaaafa",
-        "aaahaa",
-        "aaalaa",
-        "aaaana",
-        "aaanaa",
-        "aaabaa",
-        "aaaada",
-        "auaaaa",
-        "aaapaa",
-        "awaaaa",
-        "ayaaaa",
-        "adaaaa",
-        "aaavaa",
-        "aagaaa",
-        "aauaaa",
-        "abaaaa",
-        "aaadaa",
-        "aqaaaa",
-        "aaaxaa",
-        "aaaawa",
-        "aaajaa",
-        "araaaa",
-        "aahaaa",
-        "aaaaha",
-        "aacaaa",
-        "aaaara",
-        "aaoaaa",
-        "ataaaa",
-        "aaaaya",
-        "aalaaa",
-        "aaazaa",
+        "pzrooh",
+        "aaakrw",
+        "vgvkxb",
+        "ilaumf",
+        "snzsrz",
+        "qymapx",
+        "hhjgwz",
+        "mymxyu",
+        "jglmrs",
+        "yycsvl",
+        "fuyzco",
+        "ivfyfx",
+        "hzlhqt",
+        "ansstc",
+        "ujkfnr",
+        "jrekmp",
+        "himbkv",
+        "tjztqw",
+        "jmcapu",
+        "gwwwmd",
+        "ffpond",
+        "ytzssw",
+        "afyjnp",
+        "ttrfzi",
+        "xkwmsz",
+        "oavtsf",
+        "krwjwb",
+        "bkgjcs",
+        "vsigmc",
+        "qhpxxt",
+        "apzrtt",
+        "posjnv",
+        "zlatkz",
+        "zynlqc",
+        "czajmi",
+        "smmbhm",
+        "rvlxav",
+        "wkytta",
+        "dzkfer",
+        "urajfh",
+        "lsroct",
+        "gihvuu",
+        "qtnlhu",
+        "ucjgio",
+        "xyycvd",
+        "fsssoo",
+        "kdtmux",
+        "bxnppe",
+        "usucra",
+        "hvsmau",
+        "gstvvg",
+        "ypueay",
+        "qdlvod",
+        "djfbgs",
+        "mcufib",
+        "prohkc",
+        "dsqgms",
+        "eoasya",
+        "kzplbv",
+        "rcuevr",
+        "iwapqf",
+        "ucqdac",
+        "anqomr",
+        "msztnf",
+        "tppefv",
+        "mplbgz",
+        "xnskvo",
+        "euhxrh",
+        "xrqxzw",
+        "wraxvn",
+        "zjhlou",
+        "xwdvvl",
+        "dkbiys",
+        "zbtnuv",
+        "gxrhjh",
+        "ctrczk",
+        "iwylwn",
+        "wefuhr",
+        "enlcrg",
+        "eimtep",
+        "xzvntq",
+        "zvygyf",
+        "tbzmzk",
+        "xjptby",
+        "uxyacb",
+        "mbalze",
+        "bjosah",
+        "ckojzr",
+        "ihboso",
+        "ogxylw",
+        "cfnatk",
+        "zijwnl",
+        "eczmmc",
+        "uazfyo",
+        "apywnl",
+        "jiraqa",
+        "yjksyd",
+        "mrpczo",
+        "qqmhnb",
+        "xxvsbx",
     ]
     Solution().findSecretWord(words, master)
