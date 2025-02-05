c9e087ed5375d10b507a346bcb47538a7138a6db
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-05 12:33:05 +0100

    feat: leet_code 121 complete

--- python/leet_code/150_interview/121_best_time_buy_sell_stock/main.py
+++ python/leet_code/150_interview/121_best_time_buy_sell_stock/main.py
@@ -4,23 +4,17 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        aux = sorted(prices)
-        if prices == sorted(prices, reverse=True):
-            return profit
-
-        if prices.index(min(prices)) < prices.index(max(prices)):
-            return max(prices) - min(prices)
-
-        for i in range(len(prices) - 1):
-            print(i)
-            if profit >= max(prices[i:]) - min(prices[i:]):
-                break
-            if max(prices[i:]) - prices[i] > profit:
-                profit = max(prices[i:]) - prices[i]
+        lower = 100000000
+        for n in prices:
+            if n < lower:
+                lower = n
+                continue
+            if n - lower > profit:
+                profit = n - lower
 
         return profit
 
 
 if __name__ == "__main__":
-    prices = [7, 6, 4, 3, 1]
+    prices = [2, 1, 4]
     print(Solution().maxProfit(prices))
