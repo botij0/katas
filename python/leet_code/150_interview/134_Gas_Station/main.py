4950cfc4d1b9ecaa4c62aed229ff9188aacb3775
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-13 09:38:56 +0100

    feat: leet_code 134 complete + optimized

--- python/leet_code/150_interview/134_Gas_Station/main.py
+++ python/leet_code/150_interview/134_Gas_Station/main.py
@@ -3,43 +3,21 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        start = self.get_start(gas, cost)
-        print(start)
-        if start == -1:
+        if sum(gas) - sum(cost) < 0:
             return -1
 
-        tank = gas[start]
-        i = start + 1 if start < len(cost) - 1 else 0
-        j = i - 1 if i != 0 else len(gas) - 1
-        # while True:
-        #     if tank - cost[j] < 0:
-        #         return -1
-
-        #     tank += gas[i] - cost[j]
-
-        #     if i == start:
-        #         break
-        #     i = i + 1 if i + 1 < len(cost) else 0
-        #     j = j + 1 if j + 1 < len(cost) else 0
+        start = 0
+        remain_gas = 0
+        for i in range(len(gas)):
+            remain_gas += gas[i] - cost[i]
+            if remain_gas < 0:
+                remain_gas = 0
+                start = i + 1
 
         return start
 
-    def get_start(self, gas: List[int], cost: List[int]) -> int:
-        max_diff = -1
-        better_start = -1
-        for i in range(len(gas)):
-            if gas[i] - cost[i] >= 0:
-                j = i + 1 if i < len(gas) - 1 else 0
-                suma = gas[i] - cost[i] + gas[j] - cost[j]
-                if suma > max_diff:
-                    max_diff = suma
-                    better_start = i
-
-        return better_start
-
 
 if __name__ == "__main__":
-    gas = [6, 1, 4, 3, 5]
-    cost = [3, 8, 2, 4, 2]
-    print(sum(gas) - sum(cost) + (gas[4] - cost[0]))
-    # print(Solution().canCompleteCircuit(gas, cost))
+    gas = [1, 2, 3, 4, 5]
+    cost = [3, 4, 5, 1, 2]
+    print(Solution().canCompleteCircuit(gas, cost))  # 3
