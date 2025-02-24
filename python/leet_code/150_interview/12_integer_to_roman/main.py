194c3dfb01c7c8905b6eb30291299b6cafd5c2e2
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-24 10:17:29 +0100

    feat: leet_code 12 refactor

--- python/leet_code/150_interview/12_integer_to_roman/main.py
+++ python/leet_code/150_interview/12_integer_to_roman/main.py
@@ -1,52 +1,26 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         r = ""
-        while num > 0:
-            if num >= 1000:
-                r += "M"
-                num -= 1000
-            elif num >= 500:
-                if num // 900 == 1:
-                    r += "CM"
-                    num -= 900
-                else:
-                    r += "D"
-                    num -= 500
-            elif num >= 100:
-                if num // 400 == 1:
-                    r += "CD"
-                    num -= 400
-                else:
-                    r += "C"
-                    num -= 100
-            elif num >= 50:
-                if num // 90 == 1:
-                    r += "XC"
-                    num -= 90
-                else:
-                    r += "L"
-                    num -= 50
-            elif num >= 10:
-                if num // 40 == 1:
-                    r += "XL"
-                    num -= 40
-                else:
-                    r += "X"
-                    num -= 10
-            elif num >= 5:
-                if num // 9 == 1:
-                    r += "IX"
-                    num -= 9
-                else:
-                    r += "V"
-                    num -= 5
-            else:
-                if num // 4 == 1:
-                    r += "IV"
-                    num -= 4
-                else:
-                    r += "I"
-                    num -= 1
+        roman = {
+            "M": 1000,
+            "CM": 900,
+            "D": 500,
+            "CD": 400,
+            "C": 100,
+            "XC": 90,
+            "L": 50,
+            "XL": 40,
+            "X": 10,
+            "IX": 9,
+            "V": 5,
+            "IV": 4,
+            "I": 1,
+        }
+
+        for k, v in roman.items():
+            current = num // v
+            r += k * current
+            num -= current * v
         return r
 
 
