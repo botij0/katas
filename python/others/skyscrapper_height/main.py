1d9a7e960a22e02119041a0d6603d7c4d72a71b0
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-25 16:38:15 +0100

    feat: skyscrappers optimized

--- python/others/skyscrapper_height/main.py
+++ python/others/skyscrapper_height/main.py
@@ -3,17 +3,28 @@ from typing import List
 
 class Solution:
     def solution(self, A: List[int]) -> List[int]:
-        skyscrapper = []
-        for n in A:
-            self.get_max_height(skyscrapper, n)
-        return skyscrapper
+        skyscrapper = set()
+        for i in range(len(A)):
+            A[i] = self.get_max_height(skyscrapper, A[i])
+        return A
 
-    def get_max_height(self, skyscrapper: list, current_height: int):
+    def get_max_height(self, skyscrapper: set, current_height: int) -> int:
         if current_height not in skyscrapper:
-            skyscrapper.append(current_height)
-            return
+            skyscrapper.add(current_height)
+            return current_height
         else:
-            self.get_max_height(skyscrapper, current_height - 1)
+            return self.get_max_height(skyscrapper, current_height - 1)
+
+    # Evita el posible desbordamiento de Pila
+    def solution2(self, A: List[int]) -> List[int]:
+        skyscrapper = set()
+        for i in range(len(A)):
+            height = A[i]
+            while height in skyscrapper:
+                height -= 1
+            skyscrapper.add(height)
+            A[i] = height
+        return A
 
 
 if __name__ == "__main__":
