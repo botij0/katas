0f9d4aa9248080453b6bde156aad326e5e52353e
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-26 16:45:22 +0100

    feat: leet_code 151 optimized

--- python/leet_code/150_interview/151_reverse_words_string/main.py
+++ python/leet_code/150_interview/151_reverse_words_string/main.py
@@ -1,19 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = []
-        s = s.strip()
-        current_word = ""
-        for c in s:
-            if c == " ":
-                if len(current_word) > 0:
-                    words.append(current_word)
-                current_word = ""
-            else:
-                current_word += c
-        if len(current_word) > 0:
-            words.append(current_word)
-        words.reverse()
-        return " ".join(words)
+        words = [word for word in s.split(" ") if word]
+        return " ".join(reversed(words))
 
 
 if __name__ == "__main__":
