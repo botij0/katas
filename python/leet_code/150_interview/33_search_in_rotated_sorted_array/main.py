912fab7e80bc88da26b632bb617a76defdfce823
botij0 <javier.gonzalez.soldado@gmail.com>
2025-03-04 13:17:40 +0100

    refactor: leet_code 33 completed

--- python/leet_code/150_interview/33_search_in_rotated_sorted_array/main.py
+++ python/leet_code/150_interview/33_search_in_rotated_sorted_array/main.py
@@ -3,26 +3,7 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def is_rotated() -> bool:
-            return nums[0] > nums[-1]
-
-        if is_rotated():
-            return self.special_binary_search(nums, target)
-        else:
-            return self.clasical_binary_search(nums, target)
-
-    def clasical_binary_search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] == target:
-                return mid
-
-            if nums[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return -1
+        return self.special_binary_search(nums, target)
 
     def special_binary_search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
@@ -47,19 +28,6 @@ class Solution:
                 else:
                     left = mid + 1
         return -1
-
-        # def condition(value) -> bool:
-        #     return nums[value] > nums[value + 1] or nums[value] < nums[value - 1]
-
-        # left, right = 0, len(nums) - 1
-        # while left < right:
-        #     mid = left + (right - left) // 2
-        #     if condition(mid):
-        #         right = mid
-        #     else:
-        #         left = mid + 1
-
-        # return left
 
 
 if __name__ == "__main__":
