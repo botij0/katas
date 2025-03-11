5db3ec28b906ae7e40a8a6002998b7e8ce240578
botij0 <javier.gonzalez.soldado@gmail.com>
2025-03-11 16:52:57 +0100

    refactor: leet_code 67 completed

--- python/leet_code/150_interview/67_add_binary/main.py
+++ python/leet_code/150_interview/67_add_binary/main.py
@@ -1,32 +1,10 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        aux = False
-        l_a = len(a)
-        l_b = len(b)
-        r = ""
-        n = l_a if l_a <= l_b else l_b
-        for i in range(n):
-            if a[l_a - i - 1] == "0" and b[l_b - i - 1] == "0":
-                if aux:
-                    r = "1" + r
-                    aux = False
-                else:
-                    r = "0" + r
-
-            elif a[l_a - i - 1] == "1" and b[l_b - i - 1] == "1":
-                if aux:
-                    r = "1" + r
-                else:
-                    r = "0" + r
-                aux = True
-            else:
-                if aux:
-                    r = "0" + r
-                else:
-                    r = "1" + r
-
         m_bin = max(a, b, key=len)
         l_bin = len(m_bin)
+
+        r, aux = self.get_partial_result(a, b)
+
         if aux:
             dif = l_bin - len(r)
             for i in range(dif):
@@ -35,10 +13,38 @@ class Solution:
                 else:
                     break
             r = "1" + r
+
         if len(r) < l_bin:
             return m_bin[0 : l_bin - len(r)] + r
         else:
             return r
+
+    def get_partial_result(self, a: str, b: str):
+        r = ""
+        aux = False
+        l_a = len(a)
+        l_b = len(b)
+
+        n = l_a if l_a <= l_b else l_b
+        for i in range(n):
+            if a[l_a - i - 1] == b[l_b - i - 1]:
+                r = self.update_equals(r, aux)
+                aux = False if a[l_a - i - 1] == 0 else True
+            else:
+                r = self.update_not_equals(r, aux)
+        return r, aux
+
+    def update_equals(self, r: str, aux: bool):
+        if aux:
+            return "1" + r
+        else:
+            return "0" + r
+
+    def update_not_equals(self, r: str, aux: bool):
+        if aux:
+            return "0" + r
+        else:
+            return "1" + r
 
 
 if __name__ == "__main__":
