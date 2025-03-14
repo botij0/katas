5b7736b6823369e5679bfbc443fa4e2f99b6eeb5
botij0 <javier.gonzalez.soldado@gmail.com>
2025-03-14 12:25:05 +0100

    feat: leet_code 54 skeleton

---
+++ python/leet_code/150_interview/54_spiral_matrix/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        return [0]
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(Solution().spiralOrder(matrix))
