32f6f278d570e8e3c130492e3252b83c8d5a7c13
botij0 <javier.gonzalez.soldado@gmail.com>
2025-03-14 13:10:16 +0100

    feat: leet_code 54 completed and optimized

--- python/leet_code/150_interview/54_spiral_matrix/main.py
+++ python/leet_code/150_interview/54_spiral_matrix/main.py
@@ -3,9 +3,21 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        return [0]
+        r = []
+        while matrix:
+            self.get_row_and_rotate(matrix, r)
+
+        print(matrix)
+        return r
+
+    def get_row_and_rotate(self, matrix: List[List[int]], r: List[int]):
+        for n in matrix[0]:
+            r.append(n)
+        matrix[:] = matrix[1::]
+        matrix[:] = [list(row) for row in zip(*matrix)][::-1]
+        # Rotate matrix
 
 
 if __name__ == "__main__":
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
     print(Solution().spiralOrder(matrix))
