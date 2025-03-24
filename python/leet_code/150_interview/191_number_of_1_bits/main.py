f8dee85834536b9ffdc29a0313880f0ca9bd7b05
botij0 <javier.gonzalez.soldado@gmail.com>
2025-03-24 09:09:10 +0100

    feat: leet_code 191 completed

---
+++ python/leet_code/150_interview/191_number_of_1_bits/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        b_number = bin(n)
+        return b_number.count("1")
+
+
+if __name__ == "__main__":
+    n = 11
+    print(Solution().hammingWeight(n))
