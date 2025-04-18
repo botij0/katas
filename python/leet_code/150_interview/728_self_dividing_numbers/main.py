61e04ef387fee3e0946e3f0a4adddf2c699c7361
botij0 <javier.gonzalez.soldado@gmail.com>
2025-04-18 13:44:46 +0200

    refactor: leet code 728

--- python/leet_code/150_interview/728_self_dividing_numbers/main.py
+++ python/leet_code/150_interview/728_self_dividing_numbers/main.py
@@ -4,18 +4,22 @@ from typing import List
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         r = []
-        for i in range(left, right + 1):
-            aux = i
-            f = True
+
+        for num in range(left, right + 1):
+            aux = num
+            is_self_dividing = True
+
             while aux > 0:
-                j = aux % 10
-                if j == 0 or i % j != 0:
-                    f = False
+                digit = aux % 10
+
+                if digit == 0 or num % digit != 0:
+                    is_self_dividing = False
                     break
+
                 aux //= 10
 
-            if f:
-                r.append(i)
+            if is_self_dividing:
+                r.append(num)
 
         return r
 
