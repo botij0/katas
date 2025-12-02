9d0a9ee668d96def9fdb719e5935212196e6ffda
botij0 <javier.gonzalez.soldado@gmail.com>
2025-12-02 08:59:21 +0100

    feat: day 1 part 1

--- typescript/aoc2025/src/01/secretEntrance.ts
+++ typescript/aoc2025/src/01/secretEntrance.ts
@@ -1,8 +1,46 @@
 import { readFileContent } from "../utils";
 
 const main = async () => {
-  const fileContent = await readFileContent("src/01/exmaple.txt");
-  console.log(fileContent);
+  const fileContent = await readFileContent("src/01/input.txt");
+  const instructionsList = getInstructionsList(fileContent);
+
+  let dialValue = 50;
+  let password = 0;
+
+  instructionsList.map((instruction) => {
+    if (instruction.direction === "L") {
+      dialValue -= instruction.count % 100;
+      if (dialValue < 0) dialValue += 100;
+    } else {
+      dialValue += instruction.count % 100;
+      if (dialValue >= 100) dialValue -= 100;
+    }
+
+    if (dialValue === 0) password += 1;
+  });
+
+  console.log(instructionsList[122]);
+  console.log(password);
+};
+
+type Direction = "R" | "L";
+interface Instruction {
+  direction: Direction;
+  count: number;
+}
+
+const getInstructionsList = (fileContent: string): Instruction[] => {
+  const fileContentList = fileContent.split("\n");
+  let instructionsList: Instruction[] = [];
+
+  fileContentList.map((line) => {
+    instructionsList.push({
+      direction: line[0] as Direction,
+      count: Number(line.slice(1, line.length)),
+    });
+  });
+
+  return instructionsList;
 };
 
 main();
--- typescript/aoc2025/src/utils.ts
+++ typescript/aoc2025/src/utils.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 
 export const readFileContent = async (path: string) => {
-  const fileContent = await fs.readFile("./src/01/example.txt", "utf8");
+  const fileContent = await fs.readFile(path, "utf8");
   return fileContent;
 };
