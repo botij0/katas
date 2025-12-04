57477fef07550b12d4299dbfcd2f9bf1e96b5beb
botij0 <javier.gonzalez.soldado@gmail.com>
2025-12-04 15:59:57 +0100

    feat: day4 part 2

---
+++ typescript/aoc2025/src/04/part2.ts
@@ -0,0 +1,64 @@
+import { readFileContent } from "../utils";
+
+const main = async () => {
+  const fileContent = await readFileContent("src/04/input.txt");
+  const grid = getRollsOfPaperGrid(fileContent);
+
+  const nRows = grid.length;
+  const nCols = grid[0].length;
+
+  let finalResult = 0;
+  let currentResult = -1;
+
+  while (currentResult !== 0) {
+    const indexModified = [];
+    currentResult = 0;
+
+    for (let i = 0; i < nRows; i++) {
+      for (let j = 0; j < nCols; j++) {
+        if (grid[i][j] === ".") continue;
+        const val = checkAdyacents(i, j, grid);
+        if (val === 1) {
+          indexModified.push([i, j]);
+          currentResult += val;
+        }
+      }
+    }
+
+    indexModified.map(([i, j]) => {
+      grid[i][j] = ".";
+    });
+    finalResult += currentResult;
+  }
+
+  console.log(finalResult);
+  // console.log(grid);
+};
+
+const checkAdyacents = (i: number, j: number, grid: string[][]) => {
+  let count = 0;
+
+  for (let ii = i - 1; ii < i + 2; ii++) {
+    if (ii < 0 || ii >= grid.length) continue;
+    for (let jj = j - 1; jj < j + 2; jj++) {
+      if (jj < 0 || jj >= grid[0].length || (jj === j && ii === i)) continue;
+
+      if (grid[ii][jj] === "@" || grid[ii][jj] === "X") count += 1;
+
+      if (count >= 4) break;
+    }
+    if (count >= 4) break;
+  }
+  if (count < 4) {
+    grid[i][j] = "X";
+  }
+  return count >= 4 ? 0 : 1;
+};
+
+const getRollsOfPaperGrid = (fileContent: string) => {
+  return fileContent.split("\n").map((battery) => {
+    return battery.split("");
+  });
+};
+
+main();
