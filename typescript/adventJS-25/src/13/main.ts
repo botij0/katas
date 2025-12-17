ad55dc2e078b5c0453afbdfcae2a05b3a7854e34
botij0 <javier.gonzalez.soldado@gmail.com>
2025-12-17 18:55:08 +0100

    feat: initial day 13

---
+++ typescript/adventJS-25/src/13/main.ts
@@ -0,0 +1,9 @@
+type Factory = string[];
+type Result = "completed" | "broken" | "loop";
+
+function runFactory(factory: Factory): Result {
+  // Code here
+  return "completed";
+}
+
+console.log(runFactory([">>."]));
