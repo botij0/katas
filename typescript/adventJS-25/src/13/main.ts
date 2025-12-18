e59de69c56fc03236d5a3851ca17a5de1567c829
botij0 <javier.gonzalez.soldado@gmail.com>
2025-12-18 18:18:47 +0100

    feat: day 13

--- typescript/adventJS-25/src/13/main.ts
+++ typescript/adventJS-25/src/13/main.ts
@@ -3,7 +3,39 @@ type Result = "completed" | "broken" | "loop";
 
 function runFactory(factory: Factory): Result {
   // Code here
-  return "completed";
+  const moves: { [key: string]: number[] } = {
+    ">": [0, 1],
+    "<": [0, -1],
+    "^": [-1, 0],
+    v: [1, 0],
+  };
+
+  let presentPos = [0, 0];
+  const positions = new Set();
+  positions.add(presentPos.toString());
+
+  while (
+    presentPos[0] < factory.length &&
+    presentPos[0] >= 0 &&
+    presentPos[1] < factory[0].length &&
+    presentPos[1] >= 0
+  ) {
+    const current = factory[presentPos[0]][presentPos[1]];
+    if (current === ".") {
+      return "completed";
+    }
+
+    const move = moves[current];
+    presentPos = [presentPos[0] + move[0], presentPos[1] + move[1]];
+
+    if (positions.has(presentPos.toString())) return "loop";
+
+    positions.add(presentPos.toString());
+  }
+
+  return "broken";
 }
 
 console.log(runFactory([">>."]));
+console.log(runFactory([">>v", "<<<"]));
+console.log(runFactory(["v.", "^."]));
