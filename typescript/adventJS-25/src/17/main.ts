a3b8a1dd32ee93d80fa90c1cc601d94be5566c99
botij0 <javier.gonzalez.soldado@gmail.com>
2025-12-22 18:28:19 +0100

    feat: day 17

--- typescript/adventJS-25/src/17/main.ts
+++ typescript/adventJS-25/src/17/main.ts
@@ -1,11 +1,9 @@
 function hasFourLights(board: string[][]): boolean {
   // Code here
-  let lightsDict: Record<string, number> = {};
-
   const checkRow = (board: string[][], i: number, j: number) => {
-    // Check up
     let count = 0;
     const light = board[i][j];
+
     let ii = i - 1;
     while (ii > 0 && count < 3) {
       if (board[ii][j] === light) count += 1;
@@ -22,19 +20,38 @@ function hasFourLights(board: string[][]): boolean {
 
     return count >= 3 ? true : false;
   };
-  let r = false;
+
+  const checkCol = (board: string[][], i: number, j: number) => {
+    let count = 0;
+    const light = board[i][j];
+
+    let jj = j - 1;
+    while (jj > 0 && count < 3) {
+      if (board[i][jj] === light) count += 1;
+      else break;
+      jj -= 1;
+    }
+    jj = j + 1;
+
+    while (jj < board[i].length && count < 3) {
+      if (board[i][jj] === light) count += 1;
+      else break;
+      jj += 1;
+    }
+
+    return count >= 3 ? true : false;
+  };
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (board[i][j] === ".") continue;
 
-      r = checkRow(board, i, j);
+      if (checkRow(board, i, j)) return true;
+      if (checkCol(board, i, j)) return true;
     }
   }
 
-  console.log(lightsDict);
-
-  return r;
+  return false;
 }
 
 console.log(
