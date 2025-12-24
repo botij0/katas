dfcccf63aa76edc75ba6fbd31d7d157cdb29d22b
botij0 <javier.gonzalez.soldado@gmail.com>
2025-12-24 09:13:38 +0100

    refactor: day18

--- typescript/adventJS-25/src/18/main.ts
+++ typescript/adventJS-25/src/18/main.ts
@@ -3,106 +3,35 @@ function hasFourInARow(board: string[][]): boolean {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (board[i][j] === ".") continue;
-      if (checkVertical(board, i, j)) return true;
-      if (checkHorizontal(board, i, j)) return true;
-      if (checkDiagonal(board, i, j)) return true;
+      if (hasConsecutiveLights(i, j, 0, 1, board)) return true; // horizontal →
+      if (hasConsecutiveLights(i, j, 1, 0, board)) return true; // vertical ↓
+      if (hasConsecutiveLights(i, j, 1, 1, board)) return true; // diagonal ↘
+      if (hasConsecutiveLights(i, j, 1, -1, board)) return true; // diagonal ↙
     }
   }
 
   return false;
 }
 
-const checkVertical = (board: string[][], i: number, j: number) => {
-  let count = 0;
-  const light = board[i][j];
+const hasConsecutiveLights = (
+  rowIndex: number,
+  colIndex: number,
+  dRow: number,
+  dCol: number,
+  board: string[][]
+): boolean => {
+  const light = board[rowIndex][colIndex];
+  if (light === ".") return false;
 
-  // Check Up Values
-  let ii = i - 1;
-  while (ii >= 0 && count < 3) {
-    if (board[ii][j] === light) count += 1;
-    else break;
-    ii -= 1;
+  for (let step = 1; step < 4; step++) {
+    const newRowIndex = rowIndex + dRow * step;
+    const newColIndex = colIndex + dCol * step;
+
+    if (newRowIndex >= board.length || newColIndex >= board[0].length) return false;
+    if (board[newRowIndex][newColIndex] !== light) return false;
   }
 
-  // Check Down Values
-  ii = i + 1;
-  while (ii < board.length && count < 3) {
-    if (board[ii][j] === light) count += 1;
-    else break;
-    ii += 1;
-  }
-
-  return count >= 3 ? true : false;
-};
-
-const checkHorizontal = (board: string[][], i: number, j: number) => {
-  let count = 0;
-  const light = board[i][j];
-
-  // Check Left Values
-  let jj = j - 1;
-  while (jj >= 0 && count < 3) {
-    if (board[i][jj] === light) count += 1;
-    else break;
-    jj -= 1;
-  }
-
-  // Check Right Values
-  jj = j + 1;
-  while (jj < board[i].length && count < 3) {
-    if (board[i][jj] === light) count += 1;
-    else break;
-    jj += 1;
-  }
-
-  return count >= 3 ? true : false;
-};
-
-const checkDiagonal = (board: string[][], i: number, j: number) => {
-  let count = 0;
-  const light = board[i][j];
-
-  // Check up Left Values
-  let jj = j - 1;
-  let ii = i - 1;
-  while (jj >= 0 && ii >= 0 && count < 3) {
-    if (board[ii][jj] === light) count += 1;
-    else break;
-    jj -= 1;
-    ii -= 1;
-  }
-
-  // Check up Right Values
-  jj = j + 1;
-  ii = i - 1;
-  while (jj < board[i].length && ii >= 0 && count < 3) {
-    if (board[ii][jj] === light) count += 1;
-    else break;
-    jj += 1;
-    ii -= 1;
-  }
-
-  // Check down Left Values
-  jj = j - 1;
-  ii = i + 1;
-  while (jj >= 0 && ii < board.length && count < 3) {
-    if (board[i][jj] === light) count += 1;
-    else break;
-    jj -= 1;
-    ii += 1;
-  }
-
-  // Check down Right Values
-  jj = j + 1;
-  ii = i + 1;
-  while (jj < board[i].length && ii < board.length && count < 3) {
-    if (board[i][jj] === light) count += 1;
-    else break;
-    jj += 1;
-    ii += 1;
-  }
-
-  return count >= 3 ? true : false;
+  return true;
 };
 
 console.log(
